89e920dbe3fdb48e2fdb31a469277c8618d3329f
Roni Kreinin <rkreinin@clearpathrobotics.com>
2022-05-10 14:11:54 -0400

    Followbot updates - Added mobilenet_spatial_publisher node to get spatial data along with detection - Added state machine to followbot - Use spatial distance rather than box size

--- turtlebot4_python_examples/launch/followbot.launch.py
+++ turtlebot4_python_examples/launch/followbot.launch.py
@@ -25,10 +25,16 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
 
+ARGUMENTS = [
+    DeclareLaunchArgument('model', default_value='lite',
+                          choices=['standard', 'lite'],
+                          description='Turtlebot4 Model')
+]
+
 def generate_launch_description():
     pkg_turtlebot4_bringup = get_package_share_directory('turtlebot4_bringup')
     pkg_turtlebot4_description = get_package_share_directory('turtlebot4_description')
-    pkg_depthai_examples = get_package_share_directory('depthai_examples')
+    pkg_turtlebot4_cpp_examples = get_package_share_directory('turtlebot4_cpp_examples')
 
     param_file_cmd = DeclareLaunchArgument(
         'param_file',
@@ -44,28 +50,24 @@ def generate_launch_description():
         [pkg_turtlebot4_bringup, 'launch', 'robot.launch.py'])
     joy_teleop_launch_file = PathJoinSubstitution(
         [pkg_turtlebot4_bringup, 'launch', 'joy_teleop.launch.py'])
-    rplidar_launch_file = PathJoinSubstitution(
-        [pkg_turtlebot4_bringup, 'launch', 'rplidar.launch.py'])
     oakd_launch_file = PathJoinSubstitution(
-        [pkg_depthai_examples, 'launch', 'mobile_publisher.launch.py'])
+        [pkg_turtlebot4_cpp_examples, 'launch', 'mobilenet_spatial_publisher.launch.py'])
     description_launch_file = PathJoinSubstitution(
         [pkg_turtlebot4_description, 'launch', 'robot_description.launch.py']
     )
 
     standard_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([turtlebot4_robot_launch_file]),
-        launch_arguments=[('model', 'standard'),
+        launch_arguments=[('model', LaunchConfiguration('model')),
                           ('param_file', turtlebot4_param_yaml_file)])
     teleop_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([joy_teleop_launch_file]))
-    rplidar_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([rplidar_launch_file]))
     oakd_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([oakd_launch_file]),
         launch_arguments=[('tf_prefix', 'oakd_pro')])
     description_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([description_launch_file]),
-        launch_arguments=[('model', 'standard')])
+        launch_arguments=[('model', LaunchConfiguration('model'))])
 
     followbot = Node(
         package='turtlebot4_python_examples',
@@ -73,11 +75,10 @@ def generate_launch_description():
         output='screen'
     )
 
-    ld = LaunchDescription()
+    ld = LaunchDescription(ARGUMENTS)
     ld.add_action(param_file_cmd)
     ld.add_action(standard_launch)
     ld.add_action(teleop_launch)
-    ld.add_action(rplidar_launch)
     ld.add_action(oakd_launch)
     ld.add_action(description_launch)
     ld.add_action(followbot)
--- turtlebot4_python_examples/turtlebot4_python_examples/FollowBot/followbot.py
+++ turtlebot4_python_examples/turtlebot4_python_examples/FollowBot/followbot.py
@@ -19,84 +19,93 @@
 import threading
 import time
 
+from enum import Enum
+
 import rclpy
+from rclpy.action import ActionClient
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default
 
-from vision_msgs.msg import Detection2DArray
+from depthai_ros_msgs.msg import SpatialDetectionArray
 
 from turtlebot4_msgs.msg import UserLed
 
 from geometry_msgs.msg import Twist
 
-from irobot_create_msgs.msg import AudioNote, AudioNoteVector
+from irobot_create_msgs.action import Undock
+from irobot_create_msgs.msg import Dock
+
+
+# FollowBot state
+class State(Enum):
+    SEARCHING = 0
+    FOLLOWING = 1
+    TRACKING = 2
+    REVERSING = 3
+
+
+# Direction of target
+class Direction(Enum):
+    FORWARD = 0
+    FORWARD_LEFT = 1
+    FORWARD_RIGHT = 2
+    LEFT = 3
+    RIGHT = 4
+    UNKNOWN = 5
+
 
 class FollowBot(Node):
-    UNKNOWN = 0
-    LEFT = 1
-    RIGHT = 2
-    CENTER = 3
-    FORWARD_LEFT = 4
-    FORWARD_RIGHT = 5
-    STOP = 6
-
-    direction = UNKNOWN
-    previous_direction = RIGHT
     image_width = 300
     image_height = 300
-    fwd_margin = 25
-    turn_margin = 50
+    fps = 15
+    confidence = 0.80
+
+    is_docked = False
+
+    target = None
+    target_direction = Direction.UNKNOWN
+    target_distance = 0.0
+    last_target_direction = Direction.UNKNOWN
+    state = State.SEARCHING
+
+    forward_margin_px = 20
+    forward_turn_margin_px = 75
+    reverse_thresh = 1.2
+    follow_thresh = 1.6
+    thresh_hysteresis = 0.05
 
     def __init__(self):
         super().__init__('followbot')
 
-        mobilenet_sub = self.create_subscription(Detection2DArray,
-                                                     '/color/mobilenet_detections',
+        mobilenet_sub = self.create_subscription(SpatialDetectionArray,
+                                                     '/color/mobilenet_spatial_detections',
                                                      self.mobilenetCallback,
                                                      qos_profile_sensor_data)
+        dock_sub = self.create_subscription(Dock,
+                                            '/dock',
+                                            self.dockCallback,
+                                            qos_profile_sensor_data)
 
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', qos_profile_system_default)
         self.user_led_pub = self.create_publisher(UserLed, '/hmi/led', qos_profile_sensor_data)
-        self.audio_pub = self.create_publisher(AudioNoteVector, '/cmd_audio', qos_profile_sensor_data)
 
+        self.undock_action_client = ActionClient(self, Undock, '/undock')
 
-    def mobilenetCallback(self, msg: Detection2DArray):
-        if len(msg.detections) > 0:
-            for detection in msg.detections:
-                # Person detected
-                if detection.id == '15':
-                    position_x = detection.bbox.center.x
+    # Dock subscription callback
+    def dockCallback(self, msg: Dock):
+        self.is_docked = msg.is_docked
 
-                    left_boundary = position_x - detection.bbox.size_x / 2
-                    right_boundary = position_x + detection.bbox.size_x / 2
-
-                    bbox_size = detection.bbox.size_x * detection.bbox.size_y
-
-                    if left_boundary > self.image_width / 2 + self.turn_margin:
-                        self.direction = self.RIGHT
-                    elif left_boundary > self.image_width / 2 + self.fwd_margin:
-                        self.direction = self.FORWARD_RIGHT
-                    elif right_boundary < self.image_width / 2 - self.turn_margin:
-                        self.direction = self.LEFT
-                    elif right_boundary < self.image_width / 2 - self.fwd_margin:
-                        self.direction = self.FORWARD_LEFT
-                    else:
-                        if bbox_size < 50000.0:
-                            self.direction = self.CENTER
-                        else:
-                            self.direction = self.STOP
-                    return
-        else:
-            self.direction = self.UNKNOWN
-
-    def led(self, led, color, period, duty):
+    # Set User LEDs for TurtleBot 4
+    def setLed(self, led, color, period, duty):
         msg = UserLed()
         msg.led = led
         msg.color = color
         msg.blink_period = period
         msg.duty_cycle = duty
+
         self.user_led_pub.publish(msg)
 
+    # Send cmd_vel to Create 3
     def drive(self, linear_x, angular_z):
         msg = Twist()
         msg.angular.z = angular_z
@@ -104,84 +113,190 @@ class FollowBot(Node):
 
         self.cmd_vel_pub.publish(msg)
 
-    def detectAudio(self):
-        a = AudioNote()
-        a.frequency = 220
-        a.max_runtime.nanosec = 300000000
+    # Undock action
+    def undock(self):
+        self.undock_action_client.wait_for_server()
+        undock_goal_result = self.undock_action_client.send_goal(Undock.Goal())
+        if undock_goal_result.result.is_docked:
+            print('Undocking failed')
 
-        b = AudioNote()
-        b.frequency = 247
-        b.max_runtime.nanosec = 300000000
+    # Calculate direction of target relative to robot
+    def setTargetDirection(self):
+        # No target found
+        if self.target is None:
+            self.target_direction = Direction.UNKNOWN
+            return
 
-        c = AudioNote()
-        c.frequency = 262
-        c.max_runtime.nanosec = 300000000
+        # Last known direction of target
+        if self.target_direction is not Direction.UNKNOWN:
+            self.last_target_direction = self.target_direction
 
-        d = AudioNote()
-        d.frequency = 294
-        d.max_runtime.nanosec = 300000000
+        # Get distance of target to center of image
+        center_dist = self.target.bbox.center.x - self.image_width / 2
 
-        e = AudioNote()
-        e.frequency = 330
-        e.max_runtime.nanosec = 300000000
+        # Find target direction based on position in image
+        if abs(center_dist) < self.forward_margin_px:
+            self.target_direction = Direction.FORWARD
+        elif abs(center_dist) < self.forward_turn_margin_px:
+            if center_dist >= 0.0:
+                self.target_direction = Direction.FORWARD_RIGHT
+            else:
+                self.target_direction = Direction.FORWARD_LEFT
+        else:
+            if center_dist >= 0.0:
+                self.target_direction = Direction.RIGHT
+            else:
+                self.target_direction = Direction.LEFT
 
-        f = AudioNote()
-        f.frequency = 349
-        f.max_runtime.nanosec = 300000000
+    # FollowBot state machine
+    def stateMachine(self):
+        # Searching
+        if self.state == State.SEARCHING:
+            # No target visible
+            if self.target_direction == Direction.UNKNOWN:
+                if self.last_target_direction == Direction.UNKNOWN or \
+                   self.last_target_direction == Direction.RIGHT or \
+                   self.last_target_direction == Direction.FORWARD_RIGHT or \
+                   self.last_target_direction == Direction.FORWARD:
+                    self.drive(0.0, -0.75)
+                    self.setLed(0, 1, 500, 0.5)
+                    self.setLed(1, 0, 500, 0.5)
+                else:
+                    self.drive(0.0, 0.75)
+                    self.setLed(0, 0, 500, 0.5)
+                    self.setLed(1, 1, 500, 0.5)
+            else:
+                if self.target_distance < self.reverse_thresh - self.thresh_hysteresis:
+                    self.state = State.REVERSING
+                elif self.target_distance > self.follow_thresh + self.thresh_hysteresis:
+                    self.state = State.FOLLOWING
+                else:
+                    self.state = State.TRACKING
+        # Following
+        elif self.state == State.FOLLOWING:
+            # Lost track of target
+            if self.target_direction == Direction.UNKNOWN:
+                self.state = State.SEARCHING
+            else:
+                # Target is too close to follow
+                if self.target_distance < self.follow_thresh - self.thresh_hysteresis:
+                    self.state = State.TRACKING
+                else:
+                    if self.target_direction == Direction.FORWARD:
+                        self.drive(0.3, 0.0)
+                        self.setLed(0, 1, 1000, 1.0)
+                        self.setLed(1, 1, 1000, 1.0)
+                    elif self.target_direction == Direction.FORWARD_LEFT:
+                        self.drive(0.2, 0.2)
+                        self.setLed(0, 1, 1000, 1.0)
+                        self.setLed(1, 1, 1000, 0.5)
+                    elif self.target_direction == Direction.FORWARD_RIGHT:
+                        self.drive(0.2, -0.2)
+                        self.setLed(0, 1, 1000, 0.5)
+                        self.setLed(1, 1, 1000, 1.0)
+                    elif self.target_direction == Direction.RIGHT:
+                        self.drive(0.0, -0.3)
+                        self.setLed(0, 1, 1000, 0.5)
+                        self.setLed(1, 0, 1000, 0.5)
+                    else:
+                        self.drive(0.0, 0.3)
+                        self.setLed(0, 0, 1000, 0.5)
+                        self.setLed(1, 1, 1000, 0.5)
+        # Tracking
+        elif self.state == State.TRACKING:
+            # Lost track of target
+            if self.target_direction == Direction.UNKNOWN:
+                self.state = State.SEARCHING
+            else:
+                if self.target_distance < self.reverse_thresh - self.thresh_hysteresis:
+                    self.state = State.REVERSING
+                elif self.target_distance > self.follow_thresh + self.thresh_hysteresis:
+                    self.state = State.FOLLOWING
+                else:
+                    if self.target_direction == Direction.FORWARD:
+                        self.drive(0.0, 0.0)
+                        self.setLed(0, 0, 1000, 0.0)
+                        self.setLed(1, 2, 1000, 1.0)
+                    elif self.target_direction == Direction.FORWARD_LEFT:
+                        self.drive(0.0, 0.2)
+                        self.setLed(0, 1, 1000, 1.0)
+                        self.setLed(1, 1, 1000, 0.5)
+                    elif self.target_direction == Direction.FORWARD_RIGHT:
+                        self.drive(0.0, -0.2)
+                        self.setLed(0, 1, 1000, 0.5)
+                        self.setLed(1, 1, 1000, 1.0)
+                    elif self.target_direction == Direction.RIGHT:
+                        self.drive(0.0, -0.3)
+                        self.setLed(0, 1, 1000, 0.5)
+                        self.setLed(1, 0, 1000, 0.5)
+                    else:
+                        self.drive(0.0, 0.3)
+                        self.setLed(0, 0, 1000, 0.5)
+                        self.setLed(1, 1, 1000, 0.5)
+        # Reversing
+        elif self.state == State.REVERSING:
+            # Lost track of target
+            if self.target_direction == Direction.UNKNOWN:
+                self.state = State.SEARCHING
+            else:
+                if self.target_distance > self.reverse_thresh + self.thresh_hysteresis:
+                    self.state = State.TRACKING
+                else:
+                    if self.target_direction == Direction.FORWARD:
+                        self.drive(-0.2, 0.0)
+                        self.setLed(0, 0, 1000, 0.0)
+                        self.setLed(1, 2, 1000, 1.0)
+                    elif self.target_direction == Direction.FORWARD_LEFT:
+                        self.drive(-0.2, 0.2)
+                        self.setLed(0, 1, 1000, 1.0)
+                        self.setLed(1, 1, 1000, 0.5)
+                    elif self.target_direction == Direction.FORWARD_RIGHT:
+                        self.drive(-0.2, -0.2)
+                        self.setLed(0, 1, 1000, 0.5)
+                        self.setLed(1, 1, 1000, 1.0)
+                    elif self.target_direction == Direction.RIGHT:
+                        self.drive(-0.1, -0.3)
+                        self.setLed(0, 1, 1000, 0.5)
+                        self.setLed(1, 0, 1000, 0.5)
+                    else:
+                        self.drive(-0.1, 0.3)
+                        self.setLed(0, 0, 1000, 0.5)
+                        self.setLed(1, 1, 1000, 0.5)
 
-        g = AudioNote()
-        g.frequency = 392
-        g.max_runtime.nanosec = 300000000
-
-        beep = AudioNote()
-        beep.frequency = 1760
-        beep.max_runtime.nanosec = 400000000
-
-        anotes = AudioNoteVector()
-        #anotes.notes = [a, b, c, d, e, f, g]
-        anotes.notes = [beep, beep, beep]
-        self.audio_pub.publish(anotes)
+    # Callback for mobilenet spatial detection subscription
+    def mobilenetCallback(self, msg: SpatialDetectionArray):
+        closest_target_dist = self.image_width
+        target = None
+        if len(msg.detections) > 0:
+            for detection in msg.detections:
+                # Person detected
+                if detection.results[0].class_id == '15' and \
+                   detection.results[0].score > self.confidence:
+                    # No one previously detected, target first detection
+                    if self.target is None:
+                        target = detection
+                        break
+                    # Find closest detection to previous target
+                    if abs(self.target.bbox.center.x - detection.bbox.center.x) < \
+                       closest_target_dist:
+                        closest_target_dist = abs(
+                            self.target.bbox.center.x - detection.bbox.center.x)
+                        target = detection
+        # Get target distance
+        if target is not None:
+            self.target_distance = target.position.z
+        self.target = target
+        self.setTargetDirection()
 
     def run(self):
+        # Undock first
+        if self.is_docked:
+            print('Undocking')
+            self.undock()
+
         while True:
-            if self.direction == self.STOP:
-                self.drive(0.0, 0.0)
-                self.led(0, 0, 1000, 0.0)
-                self.led(1, 2, 1000, 1.0)
-            elif self.direction == self.CENTER:
-                self.drive(0.3, 0.0)
-                self.led(0, 1, 1000, 1.0)
-                self.led(1, 1, 1000, 1.0)
-            elif self.direction == self.LEFT:
-                self.drive(0.0, 0.35)
-                self.previous_direction = self.LEFT
-                self.led(0, 0, 1000, 0.5)
-                self.led(1, 1, 1000, 0.5)
-            elif self.direction == self.RIGHT:
-                self.drive(0.0, -0.35)
-                self.previous_direction = self.RIGHT
-                self.led(0, 1, 1000, 0.5)
-                self.led(1, 0, 1000, 0.5)
-            elif self.direction == self.FORWARD_LEFT:
-                self.drive(0.2, 0.25)
-                self.previous_direction = self.LEFT
-                self.led(0, 1, 1000, 1.0)
-                self.led(1, 1, 1000, 0.5)
-            elif self.direction == self.FORWARD_RIGHT:
-                self.drive(0.2, -0.25)
-                self.previous_direction = self.LEFT
-                self.led(0, 1, 1000, 0.5)
-                self.led(1, 1, 1000, 1.0)
-            else:
-                if self.previous_direction == self.LEFT:
-                    self.drive(0.0, 0.75)
-                    self.led(0, 0, 500, 0.5)
-                    self.led(1, 1, 500, 0.5)
-                else:
-                    self.drive(0.0, -0.75)
-                    self.led(0, 1, 500, 0.5)
-                    self.led(1, 0, 500, 0.5)
-            time.sleep(0.05)
+            self.stateMachine()
+            time.sleep(1/self.fps)
 
 
 def main(args=None):
@@ -189,8 +304,12 @@ def main(args=None):
 
     node = FollowBot()
 
+    # Spin rclpy on separate thread
     thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
     thread.start()
+
+    # Allow time for other nodes to start
+    time.sleep(5)
 
     print('Running FollowBot...\n')
 
